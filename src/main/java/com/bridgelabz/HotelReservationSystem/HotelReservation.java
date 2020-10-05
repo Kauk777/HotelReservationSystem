2e3f39bc641922e3c782cf35afa570e9b65a7bd3
Kauk777 <curokky777@gmail.com>
2020-10-06 00:43:00 +0530

    [KAUSHAL]Added: Welcome message

---
+++ src/main/java/com/bridgelabz/HotelReservationSystem/HotelReservation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.HotelReservationSystem;
+
+public class HotelReservation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to hotel reservation program" );
+    }
+}
